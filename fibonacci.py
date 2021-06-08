4b4a977b347bfe46c2446f8b17c980aad26f2ed0
Laura Iacconi <laura.iacconi@port.ac.uk>
2021-06-08 11:20:43 +0200

    Add cache to fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,10 @@
+from functools import cache
+
+@cache 
 def fibonacci(n):
     if n==0:
         return 0
     if n==1:
         return 1
     return fibonacci(n-1)+fibonacci(n-2) 
-print(fibonacci(20))
+print(fibonacci(35))
